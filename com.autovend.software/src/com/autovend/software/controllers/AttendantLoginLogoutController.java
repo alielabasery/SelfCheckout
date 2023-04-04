028e45b9cddcf149f292c8f2064c027e127ae3e1
Dante <dkirsman@gmail.com>
2023-04-04 10:26:32 -0600

    Added login and logout

---
+++ com.autovend.software/src/com/autovend/software/controllers/AttendantLoginLogoutController.java
@@ -0,0 +1,28 @@
+package com.autovend.software.controllers;
+
+import com.autovend.devices.SimulationException;
+
+import java.util.HashMap;
+
+public class AttendantLoginLogoutController {
+    private Boolean attendantLoggedIn = false;
+    public static final HashMap<String, String> idAndPasswords = new HashMap<>();
+    public void AttendantLogin(String attendantId, String password) {
+        if (!idAndPasswords.containsKey(attendantId)) {
+            throw new SimulationException("ID does not exist in the system!");
+        }
+        if (!idAndPasswords.containsKey(password)) {
+            throw new SimulationException("Wrong password!");
+        }
+        attendantLoggedIn = true;
+    }
+
+    public void AttendantLogout() {
+        if (attendantLoggedIn) {attendantLoggedIn = false;}
+        else {throw new SimulationException("Nobody is logged in!");}
+    }
+
+    public Boolean getLoggedIn() {
+        return attendantLoggedIn;
+    }
+}
