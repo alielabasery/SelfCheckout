8e25565eb30764cbf82ab2b1990ef3b4d99e122d
Zoe Kirsman <zoe.kirsman@ucalgary.ca>
2023-04-04 14:02:34 -0600

    added to AttendantLoginLogoutTest

--- com.autovend.software.test/src/com/autovend/software/test/AttendantLoginLogoutTest.java
+++ com.autovend.software.test/src/com/autovend/software/test/AttendantLoginLogoutTest.java
@@ -3,8 +3,12 @@ package com.autovend.software.test;
 import com.autovend.devices.SimulationException;
 import com.autovend.software.controllers.AttendantLoginLogoutController;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import org.junit.After;
 import org.junit.Before;
@@ -16,17 +20,17 @@ public class AttendantLoginLogoutTest {
     //member variables
     private AttendantLoginLogoutController attendantLoginLogoutController;
 
-    private static final String attendant1Id = "...";
-    private static final String attendant1Password = "...";
-    private static final String attendant2Id = "...";
-    private static final String attendant2Password = "...";
-    private static final String attendant3Id = "...";
-    private static final String attendant3Password = "...";
+    private static final String attendant1Id = "001";
+    private static final String attendant1Password = "Attendant1!";
+    private static final String attendant2Id = "002";
+    private static final String attendant2Password = "SelfCheck2@";
+    private static final String attendant3Id = "003";
+    private static final String attendant3Password = "Station3$";
 
-    private static final String idNotInDatabase = "...";
-    private static final String passwordNotInDatabase = "...";
-    
+    private static final String idNotInDatabase = "004";
+    private static final String passwordNotInDatabase = "Access4#";
 
+    ByteArrayOutputStream outContent; //to capture output stream
 
     @BeforeClass
     public static void setUpClass() {
@@ -39,11 +43,19 @@ public class AttendantLoginLogoutTest {
     @Before
     public void setUp() {
         attendantLoginLogoutController = new AttendantLoginLogoutController();
+
+        //redirecting the output stream to ByteArrayOutputStream called outContent
+        outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
     }
 
     @After
     public void tearDown() {
         attendantLoginLogoutController = null;
+
+        //reset output stream
+        System.setOut(System.out); 
+        outContent = null;
     }
 
     @Test
@@ -56,18 +68,27 @@ public class AttendantLoginLogoutTest {
     public void testInvalidID() {
         attendantLoginLogoutController.AttendantLogin(idNotInDatabase, attendant1Password);
         //check message 
+        String expectedOutput = "Incorrect ID!";
+        String actualOutput = outContent.toString();
+        assertEquals(expectedOutput, actualOutput);
     }
 
     @Test (expected = SimulationException.class)
     public void testInvalidPasswordNotInDatabase() {
         attendantLoginLogoutController.AttendantLogin(attendant1Id, passwordNotInDatabase);
         //check message
+        String expectedOutput = "Incorrect password!";
+        String actualOutput = outContent.toString();
+        assertEquals(expectedOutput, actualOutput);
     }
 
     @Test (expected = SimulationException.class)
     public void testInvalidPasswordInDatabase() {
         attendantLoginLogoutController.AttendantLogin(attendant1Id, attendant3Password);
         //check message
+        String expectedOutput = "Incorrect password!";
+        String actualOutput = outContent.toString();
+        assertEquals(expectedOutput, actualOutput);
 
     }
 
@@ -75,6 +96,9 @@ public class AttendantLoginLogoutTest {
     public void testInvalidIDandPassword() {
         attendantLoginLogoutController.AttendantLogin(idNotInDatabase, passwordNotInDatabase);
         //check message
+        String expectedOutput = "Incorrect ID and password!";
+        String actualOutput = outContent.toString();
+        assertEquals(expectedOutput, actualOutput);
     }
 
 
@@ -90,5 +114,8 @@ public class AttendantLoginLogoutTest {
         //didn't log in 
         attendantLoginLogoutController.AttendantLogout();
         //check message
+        String expectedOutput = "Nobody is logged in!";
+        String actualOutput = outContent.toString();
+        assertEquals(expectedOutput, actualOutput);
     }
 }
