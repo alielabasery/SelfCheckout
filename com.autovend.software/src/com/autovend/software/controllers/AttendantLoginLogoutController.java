5a0d3fbee55867bf7976f33765ce34d7fa496046
Zoe Kirsman <zoe.kirsman@ucalgary.ca>
2023-04-04 12:17:11 -0600

    created tests for AttendantLoginLogoutController

--- com.autovend.software/src/com/autovend/software/controllers/AttendantLoginLogoutController.java
+++ com.autovend.software/src/com/autovend/software/controllers/AttendantLoginLogoutController.java
@@ -7,11 +7,14 @@ public class AttendantLoginLogoutController {
     private Boolean attendantLoggedIn = false;
     public static final HashMap<String, String> idAndPasswords = new HashMap<>();
     public void AttendantLogin(String attendantId, String password) {
+        if (!idAndPasswords.containsKey(attendantId) && !(idAndPasswords.get(attendantId) == password)) {
+            throw new SimulationException("Incorrect ID and password!");
+        }
         if (!idAndPasswords.containsKey(attendantId)) {
-            throw new SimulationException("ID does not exist in the system!");
+            throw new SimulationException("Incorrect ID!");
         }
         if (!(idAndPasswords.get(attendantId) == password)) {
-            throw new SimulationException("Wrong password!");
+            throw new SimulationException("Incorrect password!");
         }
         attendantLoggedIn = true;
     }
