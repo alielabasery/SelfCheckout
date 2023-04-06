3b7807ed6cf45f2685e03c3d1bbc67905d9a887a
JPeraless <jalfperi@hotmail.com>
2023-04-06 11:01:44 -0600

    Mostly done, need to figure out weight

--- com.autovend.software/src/com/autovend/software/controllers/AddItemByPLUController.java
+++ com.autovend.software/src/com/autovend/software/controllers/AddItemByPLUController.java
@@ -2,6 +2,8 @@ package com.autovend.software.controllers;
 
 
 import com.autovend.PriceLookUpCode;
+import com.autovend.PriceLookUpCodedUnit;
+import com.autovend.SellableUnit;
 import com.autovend.devices.TouchScreen;
 import com.autovend.devices.observers.TouchScreenObserver;
 import com.autovend.external.ProductDatabases;
@@ -35,54 +37,80 @@ public class AddItemByPLUController extends ItemAdderController<TouchScreen, Tou
     }
 
     public void reactToAddByPLUCodeEvent(TouchScreen touchScreen) {
-        String pluString = "";
-
-        // set to true if the user wants to exit the process of entering a PLU code
-        Boolean exit = false;
-
         // 3. Customer I/O: Signals to the System that an item with a given PLU code is being added.
         // 4. System: Blocks the self-checkout station from further input.
         disableDevice();
 
         // 1. Customer I/O: Displays a virtual numeric keyboard.
         // 1. Simple mechanisms for self-correction by the customer, like a backspace key, should be supported.
-        pluString, exit = displayNumericKeyboard();  // would return the PlU code entered by the user, and bool exit
+        // displayNumericKeyboard() return type: return pluString + "," + exit;
+        String result = displayNumericKeyboard();  // provided by GUI team
+        String[] values = result.split(",");
+        String pluString = values[0];
+        boolean exit = Boolean.parseBoolean(values[1]);  // true if user wants to exit the process of entering a PLU code
 
-        if (exit)
+        if (exit) {
+            enableDevice();
+            // resetGUI() executes point no. 8
+            resetGUI();  // provided by GUI team
             return;
+        }
 
         PriceLookUpCode pluCode = BarcodeUtils.stringPLUToPLU(pluString);
 
-        PLUCodedProduct item = ProductDatabases.PLU_PRODUCT_DATABASE.get(pluCode);
 
-        if (item != null) {
-            this.getMainController().addItem(this, item, item.getExpectedWeight());
+
+        PLUCodedProduct product = ProductDatabases.PLU_PRODUCT_DATABASE.get(pluCode);
+
+        // trying to get the weight from pluCode and product
+        // PriceLookUpCodedUnit unit = PriceLookUpCodedUnit
+
+        if (product != null) {
+            // 6. Bagging Area: Signals the System that the weight has changed.
+            this.getMainController().addItem(this, product, 1.0);
         }
         else {
             while (!ProductDatabases.PLU_PRODUCT_DATABASE.containsKey(pluCode)) {
 
-                if (exit)
-                    break;
-
                 // 2. The customer ought to have the opportunity to see that the PLU does not correspond to their item.
                 // At this point, communication with the attendant is likely the best option for correcting the problem.
+                // incorrectPLUCode() executes point no. 2
+                incorrectPLUCode();  // provided by GUi team
 
-                pluString, exit = displayNumericKeyboard();
+                result = displayNumericKeyboard();  // provided by GUI team
+                values = result.split(",");
+                pluString = values[0];
+                exit = Boolean.parseBoolean(values[1]);  // true if user wants to exit the process of entering a PLU code
+
+                if (exit) {
+                    enableDevice();
+                    // resetGUI() executes point no. 8
+                    resetGUI();  // provided by GUI team
+                    return;
+                }
+
                 pluCode = BarcodeUtils.stringPLUToPLU(pluString);
+
+                product = ProductDatabases.PLU_PRODUCT_DATABASE.get(pluCode);
+
+                // trying to get the weight from pluCode and product
+                // unit = PriceLookUpCodedUnit
             }
+            // 6. Bagging Area: Signals the System that the weight has changed.
+            this.getMainController().addItem(this, product, 1.0);
+
         }
 
         // 5. Customer I/O: Signals to the customer to add the item to the Bagging Area.
-
-
-        // 6. Bagging Area: Signals the System that the weight has changed.
-
+        // signalToAddToBaggingArea() executes point no. 5
+        signalToAddToBaggingArea();  // provided by GUI team
 
         // 7. System: Unblocks the station.
         enableDevice();
 
         // 8. Customer I/O: Returns to its standard display.
-        resetGUI();
+        // resetGUI() executes point no. 8
+        resetGUI();  // provided by GUi team
     }
 
 }
