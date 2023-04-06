40d3c44ed6ecb10ac45c3b6d1ebaedaa50420681
fizzlewiththesizzle <faisy@hotmail.ca>
2023-04-06 12:33:55 -0600

    created reactToAddByBrowseEvent, implemented uncreated GUI methods

--- com.autovend.software/src/com/autovend/software/controllers/AddItemByBrowsingController.java
+++ com.autovend.software/src/com/autovend/software/controllers/AddItemByBrowsingController.java
@@ -5,14 +5,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.autovend.BarcodedUnit;
+import com.autovend.devices.SimulationException;
+import com.autovend.devices.TouchScreen;
+import com.autovend.devices.observers.TouchScreenObserver;
 import com.autovend.external.ProductDatabases;
 import com.autovend.products.BarcodedProduct;
 
-public class AddItemByBrowsingController {
+public class AddItemByBrowsingController extends ItemAdderController<TouchScreen, TouchScreenObserver> implements TouchScreenObserver {
 	
 	List<BarcodedProduct> catalog = new ArrayList<BarcodedProduct>();
 	BarcodedUnit item; 
 	BigDecimal price;
+	double weight; 
+	BarcodedProduct barcodedProduct;
+	
+	public AddItemByBrowsingController(TouchScreen touchscreen) {
+		super(touchscreen);
+	}
 	
 	/*
 	 * Add products in database to catalog
@@ -32,8 +41,39 @@ public class AddItemByBrowsingController {
 			if(product.getDescription().toLowerCase().equals(name)) {
 				item = new BarcodedUnit(product.getBarcode(), product.getExpectedWeight());
 				price = product.getPrice().multiply(productQuantity);
+				weight = product.getExpectedWeight();
+				barcodedProduct = product;
 			}
 		}
+	}
+	
+	public void reactToAddByBrowseEvent(TouchScreen touchscreen) {
+		disableDevice();
+		
+		String entryName = displayCatalog(); //provided by GUI team, passes in string of item's name
+		String entryQuantity = selectQuantity(); //provided by GUI team, passes in string of item's quantity
+		boolean exit = cancelBrowse(); //provided by GUI team, passes in true if "Cancel/Back" is selected
+		
+		if(exit) {
+			enableDevice();
+			resetGUI();
+			return;
+		}
+		
+		//runs getProduct method above to get item information
+		getProduct(entryName, entryQuantity);
+		
+		//Signals weight change
+		if(barcodedProduct != null) {
+			this.getMainController().addItem(this, barcodedProduct, weight);
+		}
+		else {
+			throw new SimulationException("Null product!");
+		}
+		
+		signalToAddToBaggingArea(); //provided by GUI team
+		enableDevice();
+		resetGUI(); //provided by GUI team
 	}
 	
 	/*
@@ -54,5 +94,4 @@ public class AddItemByBrowsingController {
 	public BigDecimal getPrice() {
 		return price;
 	}
-
 }
