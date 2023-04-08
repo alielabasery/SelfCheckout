397b2900e3611298f0a5d479cc780fa6c522746d
ellatomlinson <poetella17@gmail.com>
2023-04-08 17:59:06 -0600

    Added functionality to AttendantLogin GUI

--- com.autovend.software/src/com/autovend/software/attendantgui/AttendantLogin.java
+++ com.autovend.software/src/com/autovend/software/attendantgui/AttendantLogin.java
@@ -1,5 +1,8 @@
 package com.autovend.software.attendantgui;
 
+import com.autovend.devices.SimulationException;
+import com.autovend.software.controllers.AttendantLoginLogoutController;
+
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
@@ -15,8 +18,15 @@ public class AttendantLogin {
     JLabel IDLabel;
     JLabel passLabel;
     JButton button;
+    JLabel failLabel;
+    private static final String attendant1Id = "001";
+    private static final String attendant1Password = "Attendant1!";
+
+    AttendantLoginLogoutController a = new AttendantLoginLogoutController();
 
     public AttendantLogin() {
+        AttendantLoginLogoutController.idAndPasswords.put(attendant1Id, attendant1Password);
+
         loginFrame = new JFrame();
         loginPanel = new JPanel();
         loginPanel.setPreferredSize(new Dimension(1280, 720));
@@ -45,6 +55,29 @@ public class AttendantLogin {
         button.setBounds(590, 450, 100, 20);
         button.setOpaque(true);
 
+        failLabel = new JLabel("The userID or password entered was invalid.");
+        failLabel.setBounds(510, 410, 300, 20);
+        failLabel.setForeground(Color.RED);
+
+        // On submit
+        button.addActionListener(e -> {
+            String userID = IDField.getText();
+            String password = passField.getText();
+            // Run the AttendantLogin method from the AttendantLoginLogoutController
+            try {
+                a.AttendantLogin(userID, password);
+                // If login information passes, go to attendant panel
+                // TODO: Go to main attendant access panel
+            }
+            catch (SimulationException s){
+                // If login failed
+                IDField.setText("");
+                passField.setText("");
+                loginPanel.add(failLabel);
+                show();
+            }
+
+        });
         loginPanel.add(loginLabel);
         loginPanel.add(IDField);
         loginPanel.add(passField);
@@ -58,6 +91,21 @@ public class AttendantLogin {
         loginFrame.setVisible(true);
         loginFrame.setResizable(false);
 
+    }
+
+    public void show(){
+        loginPanel.add(loginLabel);
+        loginPanel.add(IDField);
+        loginPanel.add(passField);
+        loginPanel.add(IDLabel);
+        loginPanel.add(passLabel);
+        loginPanel.add(button);
+
+        loginFrame.getContentPane().add(loginPanel, BorderLayout.CENTER);
+        loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        loginFrame.pack();
+        loginFrame.setVisible(true);
+        loginFrame.setResizable(false);
     }
 
     // Below is only for visualization, delete later.
