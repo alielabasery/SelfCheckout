854e271389174252551d41d518ff7ab59c06abe2
Saksham Puri <84263133+Saksham-P@users.noreply.github.com>
2023-04-11 21:04:06 -0600

    Made so one common AttendantScreen is used for login and main usage

--- com.autovend.software/src/com/autovend/software/controllers/GuiController.java
+++ com.autovend.software/src/com/autovend/software/controllers/GuiController.java
@@ -18,10 +18,13 @@ public class GuiController {
     SelfCheckoutStation station;
     SupervisionStation attendantStation;
     AttendentController attendantController;
+    
+    JFrame attendantScreen;
 
     public GuiController(SelfCheckoutStation station, SupervisionStation attendantStation) {
         this.station = station;
         this.attendantStation = attendantStation;
+        attendantScreen = this.attendantStation.screen.getFrame();
         this.attendantController = new AttendentController();
     }
 
@@ -67,43 +70,45 @@ public class GuiController {
 
     // Attendant Station
     public void attendantLoginScreen() {
-        JFrame screen = attendantStation.screen.getFrame();
-        screen.setExtendedState(JFrame.NORMAL);
-        screen.setPreferredSize(new Dimension(1280, 720));
-        screen.getContentPane().removeAll();
-        screen.setLayout(new BorderLayout());
+    	attendantScreen.setExtendedState(JFrame.NORMAL);
+    	attendantScreen.setPreferredSize(new Dimension(1280, 720));
+    	attendantScreen.getContentPane().removeAll();
+    	attendantScreen.setLayout(new BorderLayout());
         AttendantLogin al = new AttendantLogin(this);
         JPanel panel = new JPanel();
         panel.add(al);
-        screen.getContentPane().add(panel, BorderLayout.CENTER);
-        screen.pack();
+        attendantScreen.getContentPane().add(panel, BorderLayout.CENTER);
+        attendantScreen.pack();
         // Make the JFrame display in the middle of the screen
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        int x = (int) (screenSize.getWidth() - screen.getWidth()) / 2;
-        int y = (int) (screenSize.getHeight() - screen.getHeight()) / 2;
-        screen.setLocation(x, y);
-        screen.validate();
+        int x = (int) (screenSize.getWidth() - attendantScreen.getWidth()) / 2;
+        int y = (int) (screenSize.getHeight() - attendantScreen.getHeight()) / 2;
+        attendantScreen.setLocation(x, y);
+        attendantScreen.validate();
         attendantStation.screen.setVisible(true);
     }
 
     public void attendantScreen() {
-        JFrame screen = attendantStation.screen.getFrame();
-        screen.setExtendedState(JFrame.NORMAL);
-        screen.setPreferredSize(new Dimension(1280, 720));
-        screen.getContentPane().removeAll();
-        screen.setLayout(new BorderLayout());
+    	attendantScreen.setExtendedState(JFrame.NORMAL);
+    	attendantScreen.setPreferredSize(new Dimension(1280, 720));
+    	attendantScreen.getContentPane().removeAll();
+    	attendantScreen.setLayout(new BorderLayout());
         AttendantPanel ap = new AttendantPanel(this, attendantStation, attendantController);
         JPanel panel = new JPanel();
         panel.add(ap);
-        screen.getContentPane().add(panel, BorderLayout.CENTER);
-        screen.pack();
+        attendantScreen.getContentPane().add(panel, BorderLayout.CENTER);
+        attendantScreen.pack();
         // Make the JFrame display in the middle of the screen
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        int x = (int) (screenSize.getWidth() - screen.getWidth()) / 2;
-        int y = (int) (screenSize.getHeight() - screen.getHeight()) / 2;
-        screen.setLocation(x, y);
-        screen.validate();
+        int x = (int) (screenSize.getWidth() - attendantScreen.getWidth()) / 2;
+        int y = (int) (screenSize.getHeight() - attendantScreen.getHeight()) / 2;
+        attendantScreen.setLocation(x, y);
+        attendantScreen.validate();
         attendantStation.screen.setVisible(true);
+    }
+    
+    public void validateAttendantScreen() {
+    	attendantScreen.validate();
     }
 
     // Delete later
@@ -120,7 +125,7 @@ public class GuiController {
         CheckoutController cc3 = new CheckoutController(s3);
         
         NetworkController.registerCheckoutStation("Station 1", cc);
-        NetworkController.registerCheckoutStation("Station 2", cc2);
+        NetworkController.registerCheckoutStation("Station 2", cc2);	
         NetworkController.registerCheckoutStation("Station 3", cc3);
         
         SupervisionStation ss = new SupervisionStation();
