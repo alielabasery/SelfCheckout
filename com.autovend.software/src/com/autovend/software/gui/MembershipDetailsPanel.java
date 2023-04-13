3b4681418d9ad327e6c2ffb05e12da3848b06eb0
Umesho23 <96855526+Umesho23@users.noreply.github.com>
2023-04-12 19:16:32 -0600

    Added a virtual numpad

--- com.autovend.software/src/com/autovend/software/gui/MembershipDetailsPanel.java
+++ com.autovend.software/src/com/autovend/software/gui/MembershipDetailsPanel.java
@@ -48,22 +48,34 @@ import com.autovend.software.controllers.CheckoutController;
 import com.autovend.software.controllers.GuiController;
 import com.autovend.software.controllers.MembershipCardController;
 
-public class MembershipDetailsPanel extends JPanel{
-    MembershipCardController mcc;
-    GuiController gc;
-    JLabel membershipByTypingLabel;
-    JTextField IDField;
-    JLabel IDLabel;
-    JButton button;
-    JButton backButton;
-    JLabel failLabel;
-    String userID;
-
-    
-    public MembershipDetailsPanel(GuiController gc, MembershipCardController mcc, CheckoutController controller) {
+public class MembershipDetailsPanel extends JPanel {
+	MembershipCardController mcc;
+	GuiController gc;
+	JLabel membershipByTypingLabel;
+	JTextField IDField;
+	JLabel IDLabel;
+	JButton button;
+	JButton backButton;
+	JLabel failLabel;
+	String userID;
+	JButton zero;
+	JButton one;
+	JButton two;
+	JButton three;
+	JButton four;
+	JButton five;
+	JButton six;
+	JButton seven;
+	JButton eight;
+	JButton nine;
+	private String vkeyboard = "";
+	
+	public MembershipDetailsPanel(GuiController gc, MembershipCardController mcc, CheckoutController controller) {
         this.gc = gc;
         this.mcc = mcc;
+        
 
+        
         setPreferredSize(new Dimension(1280, 720));
         setLayout(null);
 
@@ -101,30 +113,178 @@ public class MembershipDetailsPanel extends JPanel{
         });
 
         add(backButton);
+       
 
         failLabel = new JLabel("The membership number entered was invalid");
-        failLabel.setBounds(510, 410, 300, 20);
+        failLabel.setBounds(510, 365, 300, 20);
         failLabel.setForeground(Color.RED);
-
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                userID = IDField.getText();
-                try {
-                    Boolean success = mcc.isValid(userID);
-                    if (success) { gc.addItemsScreen(controller); }
-                    remove(failLabel);
-                    revalidate();
-                    repaint();
-                } catch (IllegalDigitException s) {
-                    IDField.setText("");
-                    add(failLabel);
-                    revalidate();
-                    repaint();
-                }
-            }
+        
+        zero = new JButton("0");
+        zero.setBounds(630, 460, 20, 20);
+        zero.setBackground(Color.decode("#ade89b"));
+        zero.setForeground(Color.BLACK);
+        zero.setBorder(new LineBorder(Color.BLACK, 1, true));
+        zero.setOpaque(true);
+        zero.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		vkeyboard += "0";
+        		IDField.setText(vkeyboard);
+        	}
         });
+        
+        one = new JButton("1");
+        one.setBounds(610, 440, 20, 20);
+        one.setBackground(Color.decode("#ade89b"));
+        one.setForeground(Color.BLACK);
+        one.setBorder(new LineBorder(Color.BLACK, 1, true));
+        one.setOpaque(true);
+        one.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		vkeyboard += "1";
+        		IDField.setText(vkeyboard);	
+        	}
+        });
+        
+        two = new JButton("2");
+        two.setBounds(630, 440, 20, 20);
+        two.setBackground(Color.decode("#ade89b"));
+        two.setForeground(Color.BLACK);
+        two.setBorder(new LineBorder(Color.BLACK, 1, true));
+        two.setOpaque(true);
+        two.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		vkeyboard += "2";
+        		IDField.setText(vkeyboard);
+        	}
+        });
+        
+		three = new JButton("3");
+		three.setBounds(650, 440, 20, 20);
+		three.setBackground(Color.decode("#ade89b"));
+		three.setForeground(Color.BLACK);
+		three.setBorder(new LineBorder(Color.BLACK, 1, true));
+		three.setOpaque(true);
+		three.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				vkeyboard += "3";
+				IDField.setText(vkeyboard);
+			}
+		});
 
+		four = new JButton("4");
+		four.setBounds(610, 420, 20, 20);
+		four.setBackground(Color.decode("#ade89b"));
+		four.setForeground(Color.BLACK);
+		four.setBorder(new LineBorder(Color.BLACK, 1, true));
+		four.setOpaque(true);
+		four.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				vkeyboard += "4";
+				IDField.setText(vkeyboard);
+				
+			}
+		});
+
+		five = new JButton("5");
+		five.setBounds(630, 420, 20, 20);
+		five.setBackground(Color.decode("#ade89b"));
+		five.setForeground(Color.BLACK);
+		five.setBorder(new LineBorder(Color.BLACK, 1, true));
+		five.setOpaque(true);
+		five.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				vkeyboard += "5";
+				IDField.setText(vkeyboard);
+				
+			}
+		});
+
+		six = new JButton("6");
+		six.setBounds(650, 420, 20, 20);
+		six.setBackground(Color.decode("#ade89b"));
+		six.setForeground(Color.BLACK);
+		six.setBorder(new LineBorder(Color.BLACK, 1, true));
+		six.setOpaque(true);
+		six.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				vkeyboard += "6";
+				IDField.setText(vkeyboard);
+				
+			}
+		});
+
+		seven = new JButton("7");
+		seven.setBounds(610, 400, 20, 20);
+		seven.setBackground(Color.decode("#ade89b"));
+		seven.setForeground(Color.BLACK);
+		seven.setBorder(new LineBorder(Color.BLACK, 1, true));
+		seven.setOpaque(true);
+		seven.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				vkeyboard += "7";
+				IDField.setText(vkeyboard);
+				
+			}
+		});
+
+		eight = new JButton("8");
+		eight.setBounds(630, 400, 20, 20);
+		eight.setBackground(Color.decode("#ade89b"));
+		eight.setForeground(Color.BLACK);
+		eight.setBorder(new LineBorder(Color.BLACK, 1, true));
+		eight.setOpaque(true);
+		eight.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			vkeyboard += "8";
+			IDField.setText(vkeyboard);
+		}
+		});
+		
+		nine = new JButton("9");
+		nine.setBounds(650, 400, 20, 20);
+		nine.setBackground(Color.decode("#ade89b"));
+		nine.setForeground(Color.BLACK);
+		nine.setBorder(new LineBorder(Color.BLACK, 1, true));
+		nine.setOpaque(true);
+		nine.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			vkeyboard += "9";
+			IDField.setText(vkeyboard);
+		}
+		});
+		
+		 button.addActionListener(new ActionListener() {
+	            @Override
+	            public void actionPerformed(ActionEvent e) {
+	                userID = IDField.getText();
+	                try {
+	                    Boolean success = mcc.isValid(userID);
+	                    if (success) { gc.addItemsScreen(); }
+	                    remove(failLabel);
+	                    revalidate();
+	                    repaint();
+	                    
+	                } catch (IllegalDigitException s) {
+	                    IDField.setText("");
+	                    vkeyboard="";
+	                    add(failLabel);
+	                    revalidate();
+	                    repaint();
+	                    
+	                }
+	            }
+	        });
+	        
+        add(zero);
+        add(one);
+        add(two);
+        add(three);
+        add(four);
+        add(five);
+        add(six);
+        add(seven);
+        add(eight);
+        add(nine);
         add(membershipByTypingLabel);
         add(IDField);
         add(IDLabel);
