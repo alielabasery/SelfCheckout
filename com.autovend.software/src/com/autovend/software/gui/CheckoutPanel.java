5c61adcc9f44b13c030fc2db93007f874e38ced8
saad-elk <elkadri.saad@yahoo.com>
2023-04-12 19:01:41 -0600

    Updated CheckoutPanel.java fixed various errors

--- com.autovend.software/src/com/autovend/software/gui/CheckoutPanel.java
+++ com.autovend.software/src/com/autovend/software/gui/CheckoutPanel.java
@@ -331,24 +331,10 @@ public class CheckoutPanel extends JPanel {
                         int compare = balance.compareTo(total);
                         if (compare >= 0) {
                             giftCardInsertData.deduct(total);
-//                            lblNewLabel_2_1_1.setVisible(false);
-//                            lblNewLabel_2_1_2.setVisible(false);
-//                            lblNewLabel_2_3.setVisible(false);
-//                            lblNewLabel_2_1.setVisible(false);
-//                            lblNewLabel_2.setVisible(false);
-
                             Component[] components = frame.getContentPane().getComponents();
                             for (Component component : components) {
                                 component.setVisible(false);
                             }
-                            //btnReturn.setVisible(false);
-//                            btnFinishAndPay.setVisible(false);
-//                            txtGiftCard.setVisible(false);
-//                            lblNewLabel.setVisible(false);
-//                            cardNumber.setVisible(false);
-//                            amount.setVisible(false);
-//                            currency.setVisible(false);
-//                            pinNumber.setVisible(false);
                             successfulPaymentScreen();
                         } else {
                         	amount.setText("Not enough money");
@@ -601,26 +587,6 @@ public class CheckoutPanel extends JPanel {
 	                    int hold = cardIssuer.authorizeHold(number, amountDue);
 	
 	                    if(cardIssuer.postTransaction(number, hold, amountDue)) {
-	//                        lblNewLabel_2_1_1.setVisible(false);
-	//                        lblNewLabel_2_1_2.setVisible(false);
-	//                        lblNewLabel_2_3.setVisible(false);
-	//                        lblNewLabel_2_1.setVisible(false);
-	//                        lblNewLabel_2.setVisible(false);
-	//                        btnReturn.setVisible(false);
-	//                        btnFinishAndPay.setVisible(false);
-	//                        rdbtnDebit.setVisible(false);
-	//                        rdbtnCredit.setVisible(false);
-	//                        rdbtnNewRadioButton.setVisible(false);
-	//                        rdbtnNo.setVisible(false);
-	//                        rdbtnNo_1.setVisible(false);
-	//                        rdbtnNewRadioButton_2.setVisible(false);
-	//                        lblNewLabel.setVisible(false);
-	//                        cardNumber.setVisible(false);
-	//                        lblNewLabel_2_1_3.setVisible(false);
-	//                        lblNewLabel_2_1_4.setVisible(false);
-	//                        pinNumber.setVisible(false);
-	//                        cvv.setVisible(false);
-	//                        cardholderName.setVisible(false);
 	                        Component[] components = frame.getContentPane().getComponents();
 	                        for (Component component : components) {
 	                            component.setVisible(false);
@@ -635,11 +601,6 @@ public class CheckoutPanel extends JPanel {
                         cvv.setText("Wrong Format!");
                         pinNumber.setText("Wrong Format!");
                     }
-//                    if (type.equals("Credit")) {
-//                        creditCard = new CreditCard(type, number, cardHolder, cvvValue, pinValue, isTapEnabled, hasChip);
-//                    } else {
-//                        debitCard = new DebitCard(type, number, cardHolder, cvvValue, pinValue, isTapEnabled, hasChip);
-//                    }
                 }
             }
         });
