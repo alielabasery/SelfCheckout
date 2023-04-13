384d6d7af1a985c22d0756c95f6860feeec65329
saad-elk <elkadri.saad@yahoo.com>
2023-04-12 18:20:36 -0600

    Updated CheckoutPanel.java added functionality for all payments to advance to successfulpaymentscreen upon successful transaction

--- com.autovend.software/src/com/autovend/software/gui/CheckoutPanel.java
+++ com.autovend.software/src/com/autovend/software/gui/CheckoutPanel.java
@@ -303,6 +303,7 @@ public class CheckoutPanel extends JPanel {
         txtGiftCard.setBounds(404, 100, 400, 49);
         frame.getContentPane().add(txtGiftCard);
 
+        JButton btnReturn;
         JButton btnFinishAndPay = new JButton("Finalize Payment");
         btnFinishAndPay.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -330,7 +331,20 @@ public class CheckoutPanel extends JPanel {
                         int compare = balance.compareTo(total);
                         if (compare >= 0) {
                             giftCardInsertData.deduct(total);
-                            currency.setText("Accepted");
+                            lblNewLabel_2_1_1.setVisible(false);
+                            lblNewLabel_2_1_2.setVisible(false);
+                            lblNewLabel_2_3.setVisible(false);
+                            lblNewLabel_2_1.setVisible(false);
+                            lblNewLabel_2.setVisible(false);
+                            btnReturn.setVisible(false);
+                            btnFinishAndPay.setVisible(false);
+                            txtGiftCard.setVisible(false);
+                            lblNewLabel.setVisible(false);
+                            cardNumber.setVisible(false);
+                            amount.setVisible(false);
+                            currency.setVisible(false);
+                            pinNumber.setVisible(false);
+                            successfulPaymentScreen();
                         } else {
                             currency.setText("Declined");
                         }
@@ -348,7 +362,7 @@ public class CheckoutPanel extends JPanel {
         btnFinishAndPay.setBounds(525, 571, 357, 83);
         frame.getContentPane().add(btnFinishAndPay);
 
-        JButton btnReturn = new JButton("Return");
+        btnReturn = new JButton("Return");
         btnReturn.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 lblNewLabel_2_1_1.setVisible(false);
@@ -516,6 +530,7 @@ public class CheckoutPanel extends JPanel {
         lblNewLabel_2_3.setBounds(291, 103, 103, 49);
         frame.getContentPane().add(lblNewLabel_2_3);
 
+        JButton btnReturn;
         JButton btnFinishAndPay = new JButton("Finalize Payment");
         btnFinishAndPay.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -581,7 +596,27 @@ public class CheckoutPanel extends JPanel {
                     int hold = cardIssuer.authorizeHold(number, amountDue);
 
                     if(cardIssuer.postTransaction(number, hold, amountDue)) {
-                        cvv.setText("Went through");
+                        lblNewLabel_2_1_1.setVisible(false);
+                        lblNewLabel_2_1_2.setVisible(false);
+                        lblNewLabel_2_3.setVisible(false);
+                        lblNewLabel_2_1.setVisible(false);
+                        lblNewLabel_2.setVisible(false);
+                        btnReturn.setVisible(false);
+                        btnFinishAndPay.setVisible(false);
+                        rdbtnDebit.setVisible(false);
+                        rdbtnCredit.setVisible(false);
+                        rdbtnNewRadioButton.setVisible(false);
+                        rdbtnNo.setVisible(false);
+                        rdbtnNo_1.setVisible(false);
+                        rdbtnNewRadioButton_2.setVisible(false);
+                        lblNewLabel.setVisible(false);
+                        cardNumber.setVisible(false);
+                        lblNewLabel_2_1_3.setVisible(false);
+                        lblNewLabel_2_1_4.setVisible(false);
+                        pinNumber.setVisible(false);
+                        cvv.setVisible(false);
+                        cardholderName.setVisible(false);
+                        successfulPaymentScreen();
                     } else {
                         cvv.setText("didnt go through");
                     }
@@ -597,7 +632,7 @@ public class CheckoutPanel extends JPanel {
         btnFinishAndPay.setBounds(525, 571, 357, 83);
         frame.getContentPane().add(btnFinishAndPay);
 
-        JButton btnReturn = new JButton("Return");
+        btnReturn = new JButton("Return");
         btnReturn.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 lblNewLabel_2_1_1.setVisible(false);
@@ -835,6 +870,36 @@ public class CheckoutPanel extends JPanel {
                 BigDecimal total = new BigDecimal(totalAmount.getText());
                 if (cash.compareTo(total) >= 0) {
                     //complete
+                    lblNewLabel_2_3.setVisible(false);
+                    btnReturn.setVisible(false);
+                    btnFinishAndPay.setVisible(false);
+                    lblNewLabel.setVisible(false);
+                    label_1.setVisible(false);
+                    label_2.setVisible(false);
+                    label_3.setVisible(false);
+                    label_4.setVisible(false);
+                    label_5.setVisible(false);
+                    label_6.setVisible(false);
+                    label_7.setVisible(false);
+                    label_8.setVisible(false);
+                    label_9.setVisible(false);
+                    label_10.setVisible(false);
+                    label_11.setVisible(false);
+                    label_12.setVisible(false);
+                    label_13.setVisible(false);
+                    quantityBills100.setVisible(false);
+                    quantityBills50.setVisible(false);
+                    quantityBills20.setVisible(false);
+                    quantityBills10.setVisible(false);
+                    quantityBills5.setVisible(false);
+                    quantityBills1.setVisible(false);
+                    quantityCoins50.setVisible(false);
+                    quantityCoins25.setVisible(false);
+                    quantityCoins20.setVisible(false);
+                    quantityCoins10.setVisible(false);
+                    quantityCoins5.setVisible(false);
+                    quantityCoins1.setVisible(false);
+                    successfulPaymentScreen();
                 } else {
                     //incomplete
                 }
