89d105ec380378faab1d070f79895a485e18470f
saad-elk <elkadri.saad@yahoo.com>
2023-04-12 18:42:20 -0600

    Updated CheckoutPanel.java fixed various errors

--- com.autovend.software/src/com/autovend/software/gui/CheckoutPanel.java
+++ com.autovend.software/src/com/autovend/software/gui/CheckoutPanel.java
@@ -1,33 +1,33 @@
-/** 
-* Group Members: 
-* 
-* Ella Tomlinson (30140549)
-* Kofi Frempong (30054189) 
-* Adam Beauferris (30056865) 
-* Niran Malla (30086877)
-* Owen Tinning (30102041)
-* Victor Campos Goitia (30106934)
-* Zoe Kirsman (30113704) 
-* Youssef Abdelrhafour (30085837) 
-* James Rettie (30123362) 
-* Rezwan Ahmed (30134609)
-* Angeline Tran (30139846) 
-* Saad Elkadri (30089084) 
-* Dante Kirsman (30120778) 
-* Riyad Abdullayev (30140509)
-* Saksham Puri (30140617) 
-* Faisal Islam (30140826)
-* Naheen Kabir (30142101) 
-* Jose Perales Rivera (30143354) 
-* Aditi Yadav (30143652)
-* Sahaj Malhotra () 
-* Ali Elabasery (30148424)
-* Fabiha Fairuzz Subha (30148674) 
-* Umesh Oad (30152293)
-* Daniel Boettcher (30153811) 
-* Nam Nguyen Vu (30154892)
-* 
-*/
+/**
+ * Group Members:
+ *
+ * Ella Tomlinson (30140549)
+ * Kofi Frempong (30054189)
+ * Adam Beauferris (30056865)
+ * Niran Malla (30086877)
+ * Owen Tinning (30102041)
+ * Victor Campos Goitia (30106934)
+ * Zoe Kirsman (30113704)
+ * Youssef Abdelrhafour (30085837)
+ * James Rettie (30123362)
+ * Rezwan Ahmed (30134609)
+ * Angeline Tran (30139846)
+ * Saad Elkadri (30089084)
+ * Dante Kirsman (30120778)
+ * Riyad Abdullayev (30140509)
+ * Saksham Puri (30140617)
+ * Faisal Islam (30140826)
+ * Naheen Kabir (30142101)
+ * Jose Perales Rivera (30143354)
+ * Aditi Yadav (30143652)
+ * Sahaj Malhotra ()
+ * Ali Elabasery (30148424)
+ * Fabiha Fairuzz Subha (30148674)
+ * Umesh Oad (30152293)
+ * Daniel Boettcher (30153811)
+ * Nam Nguyen Vu (30154892)
+ *
+ */
 package com.autovend.software.gui;
 
 import java.awt.*;
@@ -335,7 +335,7 @@ public class CheckoutPanel extends JPanel {
 //                            lblNewLabel_2_3.setVisible(false);
 //                            lblNewLabel_2_1.setVisible(false);
 //                            lblNewLabel_2.setVisible(false);
-                            
+
                             Component[] components = frame.getContentPane().getComponents();
                             for (Component component : components) {
                                 component.setVisible(false);
@@ -620,7 +620,7 @@ public class CheckoutPanel extends JPanel {
 //                        pinNumber.setVisible(false);
 //                        cvv.setVisible(false);
 //                        cardholderName.setVisible(false);
-                    	Component[] components = frame.getContentPane().getComponents();
+                        Component[] components = frame.getContentPane().getComponents();
                         for (Component component : components) {
                             component.setVisible(false);
                         }
@@ -906,7 +906,7 @@ public class CheckoutPanel extends JPanel {
 //                    quantityCoins10.setVisible(false);
 //                    quantityCoins5.setVisible(false);
 //                    quantityCoins1.setVisible(false);
-                	Component[] components = frame.getContentPane().getComponents();
+                    Component[] components = frame.getContentPane().getComponents();
                     for (Component component : components) {
                         component.setVisible(false);
                     }
